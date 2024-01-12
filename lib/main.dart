82fbe52a0d3794ff61596c622799bd3af0e13d93
muxriddin <muhriddinnorqulov0604@gmail.com>
2024-01-12 14:28:22 +0500

    foreground service is complete

--- lib/main.dart
+++ lib/main.dart
@@ -7,21 +7,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_background_service/flutter_background_service.dart';
 import 'package:flutter_background_service_android/flutter_background_service_android.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:foreground_service/service.dart';
 
-const AndroidNotificationChannel channel = AndroidNotificationChannel(
-    'high_importance_channel', // id
-    'High Importance Notifications', // title
-    description:
-    'This channel is used for important notifications.', // description
-    importance: Importance.high,
-    playSound: true);
 
-final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
 
-// Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
-//   await Firebase.initializeApp();
-//   debugPrint('A bg message just showed up :  ${message.messageId}');
-// }
+
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -30,64 +20,64 @@ Future<void> main() async {
 }
 
 
-/// Foreground and Background
-Future<void> initializeService() async {
-  final service = FlutterBackgroundService();
-  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
-  FlutterLocalNotificationsPlugin();
-  if (Platform.isIOS || Platform.isAndroid) {
-    await flutterLocalNotificationsPlugin.initialize(
-      const InitializationSettings(
-        iOS: DarwinInitializationSettings(),
-        android: AndroidInitializationSettings('ic_bg_service_small'),
-      ),
-    );
-  }
-
-  await flutterLocalNotificationsPlugin
-      .resolvePlatformSpecificImplementation<
-      AndroidFlutterLocalNotificationsPlugin>()
-      ?.createNotificationChannel(channel);
-
-  await service.configure(
-    androidConfiguration: AndroidConfiguration(
-      onStart: onStart,
-      autoStart: true,
-      isForegroundMode: true,
-      notificationChannelId: 'my_foreground',
-      initialNotificationContent: 'running',
-      foregroundServiceNotificationId: 888,
-    ),
-    iosConfiguration: IosConfiguration(
-      autoStart: true,
-      onForeground: onStart,
-      onBackground: onIosBackground,
-    ),
-  );
-}
-@pragma('vm:entry-point')
-Future<bool> onIosBackground(ServiceInstance service) async {
-  WidgetsFlutterBinding.ensureInitialized();
-  DartPluginRegistrant.ensureInitialized();
-  return true;
-}
-
-@pragma('vm:entry-point')
-void onStart(ServiceInstance service) async {
-  DartPluginRegistrant.ensureInitialized();
-  if (service is AndroidServiceInstance) {
-    service.on('setAsForeground').listen((event) {
-      service.setAsForegroundService();
-    });
-    // service.on('setAsBackground').listen((event) {
-    //   service.setAsBackgroundService();
-    // });
-  }
-  service.on('stopService').listen((event) {
-    service.stopSelf();
-  });
-}
-/// Foreground and Background ///
+// /// Foreground and Background
+// Future<void> initializeService() async {
+//   final service = FlutterBackgroundService();
+//   final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+//   FlutterLocalNotificationsPlugin();
+//   if (Platform.isIOS || Platform.isAndroid) {
+//     await flutterLocalNotificationsPlugin.initialize(
+//       const InitializationSettings(
+//         iOS: DarwinInitializationSettings(),
+//         android: AndroidInitializationSettings('ic_bg_service_small'),
+//       ),
+//     );
+//   }
+//
+//   await flutterLocalNotificationsPlugin
+//       .resolvePlatformSpecificImplementation<
+//       AndroidFlutterLocalNotificationsPlugin>()
+//       ?.createNotificationChannel(channel);
+//
+//   await service.configure(
+//     androidConfiguration: AndroidConfiguration(
+//       onStart: onStart,
+//       autoStart: true,
+//       isForegroundMode: true,
+//       notificationChannelId: 'my_foreground',
+//       initialNotificationContent: 'running',
+//       foregroundServiceNotificationId: 888,
+//     ),
+//     iosConfiguration: IosConfiguration(
+//       autoStart: true,
+//       onForeground: onStart,
+//       onBackground: onIosBackground,
+//     ),
+//   );
+// }
+// @pragma('vm:entry-point')
+// Future<bool> onIosBackground(ServiceInstance service) async {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   DartPluginRegistrant.ensureInitialized();
+//   return true;
+// }
+//
+// @pragma('vm:entry-point')
+// void onStart(ServiceInstance service) async {
+//   DartPluginRegistrant.ensureInitialized();
+//   if (service is AndroidServiceInstance) {
+//     service.on('setAsForeground').listen((event) {
+//       service.setAsForegroundService();
+//     });
+//     // service.on('setAsBackground').listen((event) {
+//     //   service.setAsBackgroundService();
+//     // });
+//   }
+//   service.on('stopService').listen((event) {
+//     service.stopSelf();
+//   });
+// }
+// /// Foreground and Background ///
 
 
 class MyApp extends StatefulWidget {
@@ -102,17 +92,39 @@ class _MyAppState extends State<MyApp>{
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-          scaffoldBackgroundColor: Colors.white,
-          inputDecorationTheme: const InputDecorationTheme(
-            focusedBorder: UnderlineInputBorder(
-                borderSide: BorderSide(color: Colors.blue)
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+            scaffoldBackgroundColor: Colors.white,
+            inputDecorationTheme: const InputDecorationTheme(
+              focusedBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(color: Colors.blue)
+              ),
+            )),
+        home:  Scaffold(
+          appBar: AppBar(
+            title: const Text('Flutter Background Service'),
+          ),
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    FlutterBackgroundService().invoke("setAsForeground");
+                  },
+                  child: const Text('Set as foreground'),
+                ),
+
+                ElevatedButton(
+                  onPressed: () {
+                    FlutterBackgroundService().invoke("stopService");
+                  },
+                  child: const Text('Stop Service'),
+                ),
+              ],
             ),
-          )),
-      home:  Container(
-        color: Colors.white,
-      ),
+          ),
+        )
     );
   }
 }
--- lib/service.dart
+++ lib/service.dart
@@ -1,41 +1,76 @@
-// import 'dart:async';
-// import 'dart:io';
-// import 'dart:ui';
-//
-// import 'package:flutter/material.dart';
-// import 'package:flutter_background_service/flutter_background_service.dart';
-// import 'package:flutter_background_service_android/flutter_background_service_android.dart';
-// import 'package:flutter_local_notifications/flutter_local_notifications.dart';
-//
-// Future<void> main() async {
-//   WidgetsFlutterBinding.ensureInitialized();
-//   await initializeService();
-// }
-//
+import 'dart:async';
+import 'dart:io';
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_background_service/flutter_background_service.dart';
+import 'package:flutter_background_service_android/flutter_background_service_android.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+const channelId = "channel_id";
+
+const AndroidNotificationChannel channel = AndroidNotificationChannel(
+    channelId, // id
+    'High Importance Notifications', // title
+    description:
+    'This channel is used for important notifications.', // description
+    importance: Importance.high,
+    playSound: true
+);
+
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+
 // /// Foreground and Background
+Future<void> initializeService() async {
+  final service = FlutterBackgroundService();
+  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+  FlutterLocalNotificationsPlugin();
+  if (Platform.isIOS || Platform.isAndroid) {
+    await flutterLocalNotificationsPlugin.initialize(
+      const InitializationSettings(
+        iOS: DarwinInitializationSettings(),
+        android: AndroidInitializationSettings("app_icon"),
+      ),
+    );
+  }
+
+  await flutterLocalNotificationsPlugin
+      .resolvePlatformSpecificImplementation<
+      AndroidFlutterLocalNotificationsPlugin>()
+      ?.createNotificationChannel(channel);
+
+
+
+  await service.configure(
+    androidConfiguration: AndroidConfiguration(
+      onStart: onStart,
+      autoStart: true,
+      isForegroundMode: true,
+      notificationChannelId: channelId,
+      initialNotificationContent: 'running',
+      foregroundServiceNotificationId: 888,
+    ),
+    iosConfiguration: IosConfiguration(
+      autoStart: true,
+      onForeground: onStart,
+      onBackground: onIosBackground,
+    ),
+  );
+}
+
+
+
+
+/// Foreground and Background
 // Future<void> initializeService() async {
 //   final service = FlutterBackgroundService();
-//   final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
-//   FlutterLocalNotificationsPlugin();
-//   if (Platform.isIOS || Platform.isAndroid) {
-//     await flutterLocalNotificationsPlugin.initialize(
-//       const InitializationSettings(
-//         iOS: DarwinInitializationSettings(),
-//         android: AndroidInitializationSettings('ic_bg_service_small'),
-//       ),
-//     );
-//   }
 //
-//   await flutterLocalNotificationsPlugin
-//       .resolvePlatformSpecificImplementation<
-//       AndroidFlutterLocalNotificationsPlugin>()
-//       ?.createNotificationChannel(channel);
 //
 //   await service.configure(
 //     androidConfiguration: AndroidConfiguration(
 //       onStart: onStart,
 //       autoStart: true,
-//       isForegroundMode: false, // true is foreground and background enabled, false is only background service will enable
+//       isForegroundMode: true, // true is foreground and background enabled, false is only background service will enable
 //       notificationChannelId: 'my_foreground',
 //       initialNotificationContent: 'running',
 //       foregroundServiceNotificationId: 888,
@@ -48,31 +83,41 @@
 //   );
 //
 // }
-//
-// @pragma('vm:entry-point')
-// void onStart(ServiceInstance service) async {
-//   DartPluginRegistrant.ensureInitialized();
-//   if (service is AndroidServiceInstance) {
-//     // service.on('setAsForeground').listen((event) {
-//     //   service.setAsForegroundService();
-//     // });
-//     service.on('setAsBackground').listen((event) {
-//       service.setAsBackgroundService();
-//     });
-//   }
-//   service.on('stopService').listen((event) {
-//     service.stopSelf();
-//   });
-// }
-//
-// @pragma('vm:entry-point')
-// Future<bool> onIosBackground(ServiceInstance service) async {
-//   WidgetsFlutterBinding.ensureInitialized();
-//   DartPluginRegistrant.ensureInitialized();
-//   return true;
-// }
-//
-//
+
+@pragma('vm:entry-point')
+void onStart(ServiceInstance service) async {
+  DartPluginRegistrant.ensureInitialized();
+  if (service is AndroidServiceInstance) {
+    service.on('setAsForeground').listen((event) {
+      service.setAsForegroundService();
+    });
+    service.on('setAsBackground').listen((event) {
+      service.setAsBackgroundService();
+    });
+  }
+  service.on('stopService').listen((event) {
+    service.stopSelf();
+  });
+
+  Timer.periodic(const Duration(seconds: 1), (timer) {
+    if (service is AndroidServiceInstance) {
+      service.setForegroundNotificationInfo(
+        title: 'Flutter Background Service',
+        content: 'Updated at ${DateTime.now().second}',
+      );
+    }
+    service.invoke("update");
+  });
+}
+
+@pragma('vm:entry-point')
+Future<bool> onIosBackground(ServiceInstance service) async {
+  WidgetsFlutterBinding.ensureInitialized();
+  DartPluginRegistrant.ensureInitialized();
+  return true;
+}
+
+
 // void backgroundService()async{
 //   final service = FlutterBackgroundService();
 //   var isRunning = await service.isRunning();
